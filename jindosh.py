fd44ff629e6f1c30f387ce5a9f31c9014770f233
moorglade <moorglade@gmail.com>
2017-01-27 17:47:12 +0100

    added Condition class

--- jindosh.py
+++ jindosh.py
@@ -12,28 +12,28 @@ class CategoryAlreadyAssociated(Exception):
 
 
 class Solution(object):
-    class _Index(dict):
+    class Index(dict):
         def __init__(self, categories):
-            item_to_category = {
-                category_item: category_index
-                for category_index, category_items in enumerate(categories)
-                for category_item in category_items
+            attribute_to_category = {
+                category_attribute: category_index
+                for category_index, category_attributes in enumerate(categories)
+                for category_attribute in category_attributes
                 }
-            items = [
-                (item, (index, item_to_category[item]))
-                for index, item in enumerate(itertools.chain(*categories))
+            attributes = [
+                (attribute, (index, attribute_to_category[attribute]))
+                for index, attribute in enumerate(itertools.chain(*categories))
                 ]
 
-            dict.__init__(self, items)
+            dict.__init__(self, attributes)
             self.__category_count = len(categories)
 
-        def item_count(self):
+        def attribute_count(self):
             return len(self)
 
         def category_count(self):
             return self.__category_count
 
-    _women = [
+    _names = [
         'Lady Winslow',
         'Doctor Marcolla',
         'Countess Contee',
@@ -81,8 +81,8 @@ class Solution(object):
         'rightmost'
     ]
 
-    _index = _Index([
-        _women,
+    _index = Index([
+        _names,
         _heirlooms,
         _colors,
         _drinks,
@@ -94,36 +94,36 @@ class Solution(object):
         object.__init__(self)
 
         self.__mapping = numpy.zeros(
-            (Solution._index.item_count(), Solution._index.item_count()),
+            (Solution._index.attribute_count(), Solution._index.attribute_count()),
             dtype=bool
         )
-        self.__associated_categories = numpy.zeros(
-            (Solution._index.item_count(), Solution._index.category_count()),
+        self.__associated_categories = numpy.eye(
+            Solution._index.attribute_count(),
             dtype=bool
         )
 
         for index, category in Solution._index.itervalues():
             self.__associated_categories[index][category] = True
 
-    def associate(self, item_a, item_b):
-        item_a_index, item_a_category = Solution._index[item_a]
-        item_b_index, item_b_category = Solution._index[item_b]
+    def associate(self, attribute_a, attribute_b):
+        attribute_a_index, attribute_a_category = Solution._index[attribute_a]
+        attribute_b_index, attribute_b_category = Solution._index[attribute_b]
 
         if any([
-            self.__associated_categories[item_a_index][item_b_category],
-            self.__associated_categories[item_b_index][item_a_category]
+            self.__associated_categories[attribute_a_index][attribute_b_category],
+            self.__associated_categories[attribute_b_index][attribute_a_category]
         ]):
             raise CategoryAlreadyAssociated()
 
-        self.__mapping[item_a_index][item_b_index] = True
-        self.__mapping[item_b_index][item_a_index] = True
-        self.__associated_categories[item_a_index][item_b_category] = True
-        self.__associated_categories[item_b_index][item_a_category] = True
+        self.__mapping[attribute_a_index][attribute_b_index] = True
+        self.__mapping[attribute_b_index][attribute_a_index] = True
+        self.__associated_categories[attribute_a_index][attribute_b_category] = True
+        self.__associated_categories[attribute_b_index][attribute_a_category] = True
 
-    def is_associated(self, item_a, item_b):
-        item_a_index, _ = Solution._index[item_a]
-        item_b_index, _ = Solution._index[item_b]
-        return self.__mapping[item_a_index][item_b_index]
+    def is_associated(self, attribute_a, attribute_b):
+        attribute_a_index, _ = Solution._index[attribute_a]
+        attribute_b_index, _ = Solution._index[attribute_b]
+        return self.__mapping[attribute_a_index][attribute_b_index]
 
     def copy(self):
         return copy.deepcopy(self)
@@ -136,54 +136,81 @@ class Solution(object):
         #     pass
 
         return {
-            woman: [
+            name: [
                 heirloom
                 for heirloom in Solution._heirlooms
-                if self.is_associated(woman, heirloom)
+                if self.is_associated(name, heirloom)
                 ]
-            for woman in Solution._women
-            }
+            for name in Solution._names
+        }
 
 
-conditions = [
-    # Madam Natsiou - purple
-    [[('Madam Natsiou', 'purple')]],
+class Condition(object):
+    class Association(object):
+        def __init__(self, attribute_a, attribute_b):
+            object.__init__(self)
+            self.attribute_a = attribute_a
+            self.attribute_b = attribute_b
+
+    class Alternative(object):
+        def __init__(self, associations):
+            object.__init__(self)
+            self.associations = associations
+
+    def __init__(self, alternatives):
+        object.__init__(self)
+        self.alternatives = alternatives
+
+    def apply(self, solutions):
+        new_solutions = []
+
+        for solution in solutions:
+            for alternative, new_solution in zip(
+                self.alternatives,
+                [solution] + [solution.copy() for _ in xrange(len(self.alternatives) - 1)]
+            ):
+                try:
+                    for association in alternative.associations:
+                        new_solution.associate(
+                            association.attribute_a,
+                            association.attribute_b
+                        )
+
+                    new_solutions.append(new_solution)
+
+                except CategoryAlreadyAssociated:
+                    pass
+
+        return new_solutions
 
 
-    # Doctor Marcolla - Diamond
-    [[('Doctor Marcolla', 'Diamond')]],
-]
+    @classmethod
+    def is_same_person(cls, attribute_a, attribute_b):
+        return cls([
+            cls.Alternative([
+                cls.Association(attribute_a, attribute_b)
+            ])
+        ])
+
+
 
 
 def main():
+    conditions = [
+        Condition.is_same_person('Madam Natsiou', 'purple'),
+
+        Condition.is_same_person('Doctor Marcolla', 'Diamond')
+    ]
+
     solutions = [Solution()]
 
     for condition in conditions:
-        solutions = _apply_condition(solutions, condition)
+        solutions = condition.apply(solutions)
 
     print 'Found {} solution(s):'.format(len(solutions))
 
     for solution_index, solution in enumerate(solutions, 1):
         print '{}. {}'.format(solution_index, solution.result())
-
-
-def _apply_condition(solutions, condition):
-    new_solutions = []
-
-    for old_solution in solutions:
-        for condition_variant in condition:
-            try:
-                new_solution = old_solution if len(condition) == 1 else old_solution.copy()
-
-                for condition_item_a, condition_item_b in condition_variant:
-                    new_solution.associate(condition_item_a, condition_item_b)
-
-                new_solutions.append(new_solution)
-
-            except CategoryAlreadyAssociated:
-                pass
-
-    return new_solutions
 
 
 if __name__ == '__main__':
