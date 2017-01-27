c941e99605597ae6d7c716a0d366b5c75ea692af
moorglade <moorglade@gmail.com>
2017-01-27 20:45:58 +0100

    basic conditions

--- jindosh.py
+++ jindosh.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import copy
-import itertools
 import sys
 
 
@@ -136,184 +135,132 @@ class Solution(object):
         ]
 
 
-#         attribute_a_index, attribute_a_category = Solution._index[attribute_a]
-#         attribute_b_index, attribute_b_category = Solution._index[attribute_b]
-#
-#         if any([
-#             self.__associated_categories[attribute_a_index][attribute_b_category],
-#             self.__associated_categories[attribute_b_index][attribute_a_category]
-#         ]):
-#             raise AttributeValueAlreadyAssociated()
-#
-#         self.__mapping[attribute_a_index][attribute_b_index] = True
-#         self.__mapping[attribute_b_index][attribute_a_index] = True
-#         self.__associated_categories[attribute_a_index][attribute_b_category] = True
-#         self.__associated_categories[attribute_b_index][attribute_a_category] = True
-#
-#     def __associated_with(self, attribute):
-#         return [
-#
-#         ]
-#
-#     def __associate_pair(self, attribute_a, attribute_b):
-#         attribute_a_index, attribute_a_category = Solution._index[attribute_a]
-#         attribute_b_index, attribute_b_category = Solution._index[attribute_b]
-#
-#         if any([
-#             self.__associated_categories[attribute_a_index][attribute_b_category],
-#             self.__associated_categories[attribute_b_index][attribute_a_category]
-#         ]):
-#             raise AttributeValueAlreadyAssociated()
-#
-#         self.__mapping[attribute_a_index][attribute_b_index] = True
-#         self.__mapping[attribute_b_index][attribute_a_index] = True
-#         self.__associated_categories[attribute_a_index][attribute_b_category] = True
-#         self.__associated_categories[attribute_b_index][attribute_a_category] = True
-#
-#     def is_associated(self, attribute_a, attribute_b):
-#         attribute_a_index, _ = Solution._index[attribute_a]
-#         attribute_b_index, _ = Solution._index[attribute_b]
-#         return self.__mapping[attribute_a_index][attribute_b_index]
-#
-#     def copy(self):
-#         return copy.deepcopy(self)
-#
-#     def result(self):
-#         # while any(
-#         #         self.__fill_existing(),
-#         #         self.__fill_missing()
-#         # ):
-#         #     pass
-#
-#         return {
-#             name: [
-#                 heirloom
-#                 for heirloom in Solution._heirlooms
-#                 if self.is_associated(name, heirloom)
-#                 ]
-#             for name in Solution._names
-#         }
-#
-#
-# class Condition(object):
-#     class Association(object):
-#         def __init__(self, attribute_a, attribute_b):
-#             object.__init__(self)
-#             self.attribute_a = attribute_a
-#             self.attribute_b = attribute_b
-#
-#     class Alternative(object):
-#         def __init__(self, associations):
-#             object.__init__(self)
-#             self.associations = associations
-#
-#     def __init__(self, alternatives):
-#         object.__init__(self)
-#         self.alternatives = alternatives
-#
-#     def apply(self, solutions):
-#         new_solutions = []
-#
-#         for solution in solutions:
-#             for alternative, new_solution in zip(
-#                 self.alternatives,
-#                 [solution] + [solution.copy() for _ in xrange(len(self.alternatives) - 1)]
-#             ):
-#                 try:
-#                     for association in alternative.associations:
-#                         new_solution.associate(
-#                             association.attribute_a,
-#                             association.attribute_b
-#                         )
-#
-#                     new_solutions.append(new_solution)
-#
-#                 except AttributeValueAlreadyAssociated:
-#                     pass
-#
-#         return new_solutions
-#
-#     @classmethod
-#     def is_same_person(cls, attribute_a, attribute_b):
-#         return cls([
-#             cls.Alternative([
-#                 cls.Association(attribute_a, attribute_b)
-#             ])
-#         ])
-#
-#
+class Condition(object):
+    class Association(object):
+        def __init__(self, attribute_value_a, attribute_value_b):
+            object.__init__(self)
+            self.attribute_value_a = attribute_value_a
+            self.attribute_value_b = attribute_value_b
 
+    class Alternative(object):
+        def __init__(self, associations):
+            object.__init__(self)
+            self.associations = associations
+
+    def __init__(self, alternatives):
+        object.__init__(self)
+        self.alternatives = alternatives
+
+    def apply(self, solutions):
+        new_solutions = []
+
+        for solution in solutions:
+            for alternative, new_solution in zip(
+                self.alternatives,
+                [solution] + [solution.copy() for _ in xrange(len(self.alternatives) - 1)]
+            ):
+                try:
+                    for association in alternative.associations:
+                        new_solution.associate(
+                            association.attribute_value_a,
+                            association.attribute_value_b
+                        )
+
+                    new_solutions.append(new_solution)
+
+                except AttributeValueAlreadyAssociated:
+                    pass
+
+        return new_solutions
+
+
+def solve(attributes, conditions):
+    solutions = [Solution(attributes)]
+
+    for condition in conditions:
+        solutions = condition.apply(solutions)
+
+    for solution in solutions:
+        solution.infer()
+
+    return solutions
 
 
 # ==================================================================================================================== #
 
-riddle_attributes = [
-    Attribute(
-        'name',
-        ['Lady Winslow', 'Doctor Marcolla', 'Countess Contee', 'Madam Natsiou', 'Baroness Finch']
-    ),
-    Attribute(
-        'heirloom',
-        ['Diamond', 'Ring', 'Bird Pendant', 'War Medal', 'Snuff Tin']
-    ),
-    Attribute(
-        'color',
-        ['purple', 'red', 'green', 'white', 'blue']
-    ),
-    Attribute(
-        'drink',
-        ['beer', 'rum', 'wine', 'whiskey', 'absinthe']
-    ),
-    Attribute(
-        'city',
-        ['Dunwall', 'Dabokva', 'Baleton', 'Fraeport', 'Karnaca']
-    ),
-    Attribute(
-        'seat',
-        ['leftmost', 'center-left', 'center', 'center-right', 'rightmost']
-    )
-]
 
-#
-# conditions = [
-#     Condition.is_same_person('Madam Natsiou', 'purple'),
-#     Condition.is_same_person('Doctor Marcolla', 'Diamond')
-# ]
+def is_same_person(attribute_value_a, attribute_value_b):
+    return Condition([
+        Condition.Alternative([
+            Condition.Association(attribute_value_a, attribute_value_b)
+        ])
+    ])
+
+
+def sit_left_right(attribute_value_left, attribute_value_right):
+    seats_left_to_right = ['leftmost', 'center-left', 'center', 'center-right', 'rightmost']
+
+    return Condition([
+        Condition.Alternative([
+            Condition.Association(attribute_value_left, AttributeValue('seat', seats_left_to_right[left_seat_index])),
+            Condition.Association(attribute_value_right, AttributeValue('seat', seats_left_to_right[left_seat_index + 1]))
+        ])
+        for left_seat_index in xrange(0, len(seats_left_to_right) - 1)
+    ])
+
+
+def sit_next_to(attribute_value_a, attribute_value_b):
+    left_right_alternatives = sit_left_right(attribute_value_a, attribute_value_b).alternatives
+    right_left_alternatives = sit_left_right(attribute_value_b, attribute_value_a).alternatives
+
+    return Condition(left_right_alternatives + right_left_alternatives)
 
 
 def main():
-    solutions = [Solution(riddle_attributes)]
+    attributes = [
+        Attribute(
+            'name',
+            ['Lady Winslow', 'Doctor Marcolla', 'Countess Contee', 'Madam Natsiou', 'Baroness Finch']
+        ),
+        Attribute(
+            'heirloom',
+            ['Diamond', 'Ring', 'Bird Pendant', 'War Medal', 'Snuff Tin']
+        ),
+        Attribute(
+            'color',
+            ['purple', 'red', 'green', 'white', 'blue']
+        ),
+        Attribute(
+            'drink',
+            ['beer', 'rum', 'wine', 'whiskey', 'absinthe']
+        ),
+        Attribute(
+            'city',
+            ['Dunwall', 'Dabokva', 'Baleton', 'Fraeport', 'Karnaca']
+        ),
+        Attribute(
+            'seat',
+            ['leftmost', 'center-left', 'center', 'center-right', 'rightmost']
+        )
+    ]
 
-    solutions[0].associate(
-        AttributeValue('drink', 'beer'),
-        AttributeValue('color', 'purple')
-    )
-    solutions[0].associate(
-        AttributeValue('drink', 'rum'),
-        AttributeValue('color', 'red')
-    )
-    solutions[0].associate(
-        AttributeValue('drink', 'wine'),
-        AttributeValue('color', 'green')
-    )
-    solutions[0].associate(
-        AttributeValue('drink', 'whiskey'),
-        AttributeValue('color', 'white')
-    )
+    conditions = [
+        is_same_person(
+            AttributeValue('name', 'Madam Natsiou'),
+            AttributeValue('color', 'purple')
+        ),
+        sit_left_right(
+            AttributeValue('name', 'Madam Natsiou'),
+            AttributeValue('color', 'white')
+        )
+    ]
 
-    solutions[0].infer()
+    solutions = solve(attributes, conditions)
 
-    print solutions[0]
+    print 'Found {} solution(s):'.format(len(solutions))
 
-
-    #
-    # for condition in conditions:
-    #     solutions = condition.apply(solutions)
-    #
-    # print 'Found {} solution(s):'.format(len(solutions))
-    #
-    # for solution_index, solution in enumerate(solutions, 1):
-    #     print '{}. {}'.format(solution_index, solution.result())
+    for solution_index, solution in enumerate(solutions, 1):
+        print '{}. {}'.format(solution_index, solution)
 
 
 if __name__ == '__main__':
